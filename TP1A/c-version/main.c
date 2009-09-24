1dfdaaa5b58d53bcfd135b852eccc5bd6bef48e9
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-09-24 17:02:56 +0000

    Empty Message

--- TP1A/c-version/main.c
+++ TP1A/c-version/main.c
@@ -11,8 +11,11 @@ int main( int argc, char** argv )
 	IplImage * dst = 0;
 	IplImage * dst_ini = 0; // Para qué se debería usar??
 
-	char* filename = argc >= 2 ? argv[1] : (char*)"lena.bmp";
-	char opcion= argc==3 ? argv[2][1] : '0' ;
+	char* filename = argc == 3 ? argv[2] : (char*)"lena.bmp";
+	char opcion= argv[1][2];
+	//Me aseguro del comando...
+	if( argv[1][0] != '-' || argv[1][1] != 'r' || argv[1][2]<'1' || argv[1][2]>'5' || argv[1][3]) 
+		opcion = '6';
 
 	switch(opcion){
 		case '1':
@@ -32,13 +35,13 @@ int main( int argc, char** argv )
 			break;
 		default:
 			printf( "\nRealzador de Bordes TP1 OrgaII Grupo XoOoOoOoR (cambiar..)\n\n");
-			printf( "Uso:\n     ./bordes [filename] operador \n\n");
+			printf( "Uso:\n     ./bordes operador [filename] \n\n");
 			printf( "Operadores posibles:\n");
-			printf( "    r1: Operador de Roberts \n");
-			printf( "    r2: Operador de Prewitt\n");
-			printf( "    r3: Operador de Sobel derivando por X\n");
-			printf( "    r4: Operador de Sobel derivando por Y\n");
-			printf( "    r5: Operador de Sobel derivando por X e Y\n\n");
+			printf( "    -r1: Operador de Roberts \n");
+			printf( "    -r2: Operador de Prewitt\n");
+			printf( "    -r3: Operador de Sobel derivando por X\n");
+			printf( "    -r4: Operador de Sobel derivando por Y\n");
+			printf( "    -r5: Operador de Sobel derivando por X e Y\n\n");
 			printf( "Si no se especifica un archivo de entrada, se usara 'lena.bmp'\n\n");
 			return 0;
 	}
@@ -57,19 +60,26 @@ int main( int argc, char** argv )
 	if( (dst_ini = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) ==0 )
 		return -1;
 
-	// Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen
-	cSobel(src->imageData, dst->imageData,src->width,src->height, 1,0); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
-	cvSaveImage("derivada-x.bmp", dst);
-
-	// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
-	cSobel(src->imageData, dst->imageData,src->width,src->height, 0,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
-	cvSaveImage("derivada-y.bmp", dst);
-
-	// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
-	cSobel(src->imageData, dst->imageData,src->width,src->height, 1,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
-	cvSaveImage("full.bmp", dst);
-
+	switch(opcion) {
+		case '1':
+			//Roberts
+			break;
+		case '2':
+			//Prewitt
+			break;
+		case '3':
+			// Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen
+			cSobel(src->imageData, dst->imageData,src->width,src->height, 1,0); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
+			cvSaveImage("derivada-x.bmp", dst);
+		case '4':
+			// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
+			cSobel(src->imageData, dst->imageData,src->width,src->height, 0,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
+			cvSaveImage("derivada-y.bmp", dst);
+		case '5':
+			// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
+			cSobel(src->imageData, dst->imageData,src->width,src->height, 1,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
+			cvSaveImage("sobel.bmp", dst);
+	}
 	printf("OK\n");
 	return 0;
-
 }
