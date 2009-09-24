7b3b4c1959b6fecad679c7931bcc138f606b86d4
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-09-24 16:33:41 +0000

    Correcciones al algoritmo en C

--- trunk/TP1A/c-version/main.c
+++ trunk/TP1A/c-version/main.c
@@ -7,38 +7,69 @@
 int main( int argc, char** argv )
 {
 
-   IplImage * src = 0;
-   IplImage * dst = 0;
-   IplImage * dst_ini = 0; // Para qué se debería usar??
+	IplImage * src = 0;
+	IplImage * dst = 0;
+	IplImage * dst_ini = 0; // Para qué se debería usar??
 
-   char* filename = argc == 2 ? argv[1] : (char*)"lena.bmp";
+	char* filename = argc >= 2 ? argv[1] : (char*)"lena.bmp";
+	char opcion= argc==3 ? argv[2][1] : '0' ;
 
-   // Cargo la imagen
-   if( (src = cvLoadImage (filename, CV_LOAD_IMAGE_GRAYSCALE)) == 0 )
-	   return -1;
+	switch(opcion){
+		case '1':
+			printf( "Usando el operador de Roberts para realzar bordes\n");
+			break;
+		case '2':
+			printf( "Usando el operador de Prewitt para realzar bordes\n");
+			break;
+		case '3':
+			printf( "Usando el operador de Sobel para realzar bordes\nDerivando en X\n");
+			break;
+		case '4':
+			printf( "Usando el operador de Sobel para realzar bordes\nDerivando en Y\n"); 
+			break;
+		case '5':
+			printf( "Usando el operador de Sobel para realzar bordes\nDerivando en X e Y\n"); 
+			break;
+		default:
+			printf( "\nRealzador de Bordes TP1 OrgaII Grupo XoOoOoOoR (cambiar..)\n\n");
+			printf( "Uso:\n     ./bordes [filename] operador \n\n");
+			printf( "Operadores posibles:\n");
+			printf( "    r1: Operador de Roberts \n");
+			printf( "    r2: Operador de Prewitt\n");
+			printf( "    r3: Operador de Sobel derivando por X\n");
+			printf( "    r4: Operador de Sobel derivando por Y\n");
+			printf( "    r5: Operador de Sobel derivando por X e Y\n\n");
+			printf( "Si no se especifica un archivo de entrada, se usara 'lena.bmp'\n\n");
+			return 0;
+	}
 
-   // Creo una IplImage para cada salida esperada
-   if( (dst = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
-	   return -1;
+	// Cargo la imagen
+	if( (src = cvLoadImage (filename, CV_LOAD_IMAGE_GRAYSCALE)) == 0 ) {
+		printf("ERROR AL CARGAR LA IMAGEN %s\n", filename);
+		return -1;
+	}
 
-   // Creo una IplImage para cada salida esperada
-   if( (dst_ini = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
-	   return -1;
+	// Creo una IplImage para cada salida esperada
+	if( (dst = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
+		return -1;
 
-   // Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen 
-   cSobel(src->imageData, dst->imageData,512,512, 1,0); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
-   cvSaveImage("derivada x.BMP", dst);
+	// Creo una IplImage para cada salida esperada
+	if( (dst_ini = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) ==0 )
+		return -1;
 
-   // Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
-   cSobel(src->imageData, dst->imageData,512,512, 0,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
-   cvSaveImage("derivada y.BMP", dst);
+	// Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen
+	cSobel(src->imageData, dst->imageData,src->width,src->height, 1,0); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
+	cvSaveImage("derivada-x.bmp", dst);
 
-   // Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
-   cSobel(src->imageData, dst->imageData,512,512, 1,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
-   cvSaveImage("full.BMP", dst);
+	// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
+	cSobel(src->imageData, dst->imageData,src->width,src->height, 0,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
+	cvSaveImage("derivada-y.bmp", dst);
 
-   return 0;
+	// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
+	cSobel(src->imageData, dst->imageData,src->width,src->height, 1,1);	// Esta parte es la que tienen que programar los alumnos en ASM y comparar
+	cvSaveImage("full.bmp", dst);
+
+	printf("OK\n");
+	return 0;
 
 }
-
-
