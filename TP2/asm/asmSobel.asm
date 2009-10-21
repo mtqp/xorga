0712b8250581f6e374152d61e5c23c445e16fc0c
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-10-21 14:55:30 +0000

    no me gusto chicos

--- TP2/asm/asmSobel.asm
+++ TP2/asm/asmSobel.asm
@@ -1,10 +1,12 @@
-
+section .data
+	;sobel_mask: DW 1,2,1,1,2,1,1,2,1
+	sobel_X_linea1: DW -1,0,1,-1,0,1,-1,0
 section .text
 	global asmSobel
 
-asmRoberts:
+asmSobel:
 	convencion_C 
-
+					;no hay alineamiento xq es mult 4 imagen
 	%define line		[ebp-4]
 	%define align		4
 	%define ptr_src		[ebp+8]
@@ -16,4 +18,66 @@ asmRoberts:
 	mov esi, ptr_src		; puntero al inicio de línea src
 	mov edi, ptr_dst		; puntero al inicio de línea dst
 
+	pxor xmm7,xmm7			;acumulador parte baja
+	pxor xmm6,xmm6			;acumulador parte alta
+	movdqu xmm0,[esi]		;levanto la 1er linea
+	movdqu xmm1,xmm0
+	punpcklbw xmm0,xmm7		;tengo las primeras ocho words bajas
+	punpckhbw xmm1,xmm6		;tengo las 1ras ocho words altas
+				
+	;TRABAJO PRIMERO CON PARTE BAJA, PARTE ALTA ANALOGA
+	movdqu xmm2,xmm0		;salvo x un momento la matriz
+	movdqu xmm5,[sobel_x_linea1]
+
+	pumullw xmm0,xmm5		;[o,B,o,o,E,o,o,o]
+					;b<-a+b+c,E<-d+e+f (b y e son ceros!)
+	shufALGO xmm3,xmm0,imm8	;xmm3 == [C,fruta,fruta,F,fruta,fruta,gdaf,graf]
+				;xmm0 == [A,frefr,frefr,D,frefr,frefr,fref,fref]
+	paddw xmm0,xmm3		;xmm0 == [B,fruta,fruit,E,fruit,fruit,fruit,fru]
+	shufALGO xmm0,xmm0,imm8;;xmm0 == [frut,B,fruit,fruto,E,fruti,frtu,friut]
+	pandw xmm0,mask_borraFRUTA	;xmm0 == [0,B,0,0,E,0,0,0]
+	paddw xmm7,xmm0			;xmm7 == xmm0
+					
+	movdqu xmm0,xmm2
+	shiftDOUBLEQUADR xmm5,unaWORD	;xmm5 == [0,-1,0,1,-1,0,1,-1]
+	pmullw xmm0,xmm7		;[o,o,C,o,o,F,o,o]
+					;C<-b+c+d,F<-e+f+g	(c y f == 0)
+	shufALGO xmm3,xmm0,newimm8	;xmm0 == [-,b,-,-,e,-,-,-]
+					;xmm3 == [-,d,-,-,g,-,-,-]
+	paddw xmm0,xmm3			;xmm0 == [-,C,-,-,F,-,-,-]
+	shufALGONEW xmm0,xmm0,imm8	;xmm0 == [-,-,C,-,-,F,-,-]
+	pandw xmm0,msk_borrarFRUTNEW	;xmm0 == [0,0,C,0,0,F,0,0]
+	paddw xmm7,xmm0			;xmm7 == [0,b,c,0,e,f,0,0]
+					
+	movdqu xmm0,xmm2
+	shiftDOUBLEQUADR xmm5,otraW	;xmm5 ==  [0,0,-1,0,1,-1,0,1]
+	pmull xmm0,xmm7
+	shufALGO xmm3,xmm0,imm8
+	paddw xmm0,xmm3
+	pshufalgo2 xmm0,xmm0,imm8
+	pandw xmm0,msk_frutenborren
+	paddw xmm7,xmm0			;xmm7 == [0,b,c,d,e,f,g,0]
+					
+				;falta ver mas signific de la parte baja
+				;y el menos sig de la parte alta
+(+sigPBaja)^(-sigPAlta):		;en xmm1 estan las ocho words altas
+	movdqu xmm0,xmm2
+	pandw xmm0,DEJARDOSULTIMOS	;xmm0 == [0,0,0,0,0,0,g,h]
+	shiftDOUBLEQL xmm0,16		;xmm0 == [0,0,0,0,0,g,h,0]
+	mov xmm2,xmm1			;xmm2 == parteALTA
+	pandw xmm2,mk_dejarAB		;xmm2 == [aH,bH,0,0,0,0,0,0]
+	paddq xmm0,xmm2			;xmm0 == [aH,bH,0,0,0,g,h,0]
+	shufALGO xmm0,xmm0,imm8		;xmm0 == [h,aH,bH,0,0,g,h,aH]
+	shufAGLO xmm3,xmm0,imm8		;xmm3 == [BH,f,ff,f,f,AH,f,f]
+	pandw xmm0,xmm3			;xmm0 == [aH,f,ff,f,f,H,ff,f]
+	movdqu xmm6,xmm0
+	pandw xmm6,ffff0000000000	;xmm6 == [aH,0,0,0,0,0,0,0]
+	pshufALGO xmm0,xmm0,imm8	;xmm0 == [0,0,0,0,0,0,0,h]
+	paddw xmm7,xmm0			;xmm7 == [0,b,c,d,e,f,g,h]
+	
+	////HABRIA Q HACER LO MISMO PARA LA PARTE ALTA/////
+	////ESTO ES SOLO UNA LINEA, MEDIO Q ES UN TOQUE INEFICIENTE///
+	///FALTAN HACER LAS DOS LINEAS SIGUIENTES DE LA MATRIZ///
+	
+	
 	convencion_C_fin
