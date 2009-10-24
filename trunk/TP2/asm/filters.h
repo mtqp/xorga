6bbbe632a2f882afead829709962c5b9cd690140
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-10-24 19:34:37 +0000

    Empty Message

--- trunk/TP2/asm/filters.h
+++ trunk/TP2/asm/filters.h
@@ -2,18 +2,18 @@
 #define __FILTERS_H__
 
 /** toCharS - convierte x a char mediante saturación **/
-#define toCharS(x) x > 255? 255 : ( x < 0 ? 0 : x )
+//#define toCharS(x) x > 255? 255 : ( x < 0 ? 0 : x )
 
 /** lineSize - obtiene el tamaño de la línea incluyendo el alineado  **/
-#define lineSize(width,align) width-width%align+align*(width%align>0)
+//#define lineSize(width,align) width-width%align+align*(width%align>0)
 
-void cSobel( const unsigned char* src, unsigned char *dst, int ancho, int alto, int xorder, int yorder );
+//void cSobel( const unsigned char* src, unsigned char *dst, int ancho, int alto, int xorder, int yorder );
 /*extern asmSobel  ( const unsigned char* src, unsigned char *dst, int ancho, int alto, int xorder, int yorder );
 extern asmRoberts( const unsigned char* src, unsigned char *dst, int ancho, int alto, int xorder, int yorder );
 extern asmPrewitt( const unsigned char* src, unsigned char *dst, int ancho, int alto, int xorder, int yorder );
 //int apply_mask( const unsigned char* src, unsigned int line, const char *mask, unsigned int mask_sz );*/
 
-extern int apply_mask( const unsigned char* src, unsigned int line, const char *mask, unsigned int mask_sz );
+//extern int apply_mask( const unsigned char* src, unsigned int line, const char *mask, unsigned int mask_sz );
 
 typedef void(FILTERFN)( const unsigned char* src, unsigned char* dst, int width, int height, int xorder, int yorder );
 extern FILTERFN asmSobel;
