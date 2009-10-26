4bb16feecc9718d73f83cc2f2841bae6dce9605e
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-10-26 01:23:22 +0000

    NO PUEDE SER Q HAYA FRANCO

--- TP2/test-mariano/main_matrices.c
+++ TP2/test-mariano/main_matrices.c
@@ -1,13 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern void pm(char* ptr_src,char* ptr_dst);
+extern void apply_lineX(char* ptr_src,int selector_line);
+extern void save_mem(char* ptr_dst);
 
 int main(){
 	srand(time(NULL));
 	char * ptr_src = malloc(16);
+	ptr_src[0]= 10;
+	ptr_src[1]= 33;
+	ptr_src[2]= 24;
+	ptr_src[3]= 4;
+	ptr_src[4]= 67;
+	ptr_src[5]= 54;
+	ptr_src[6]= 34;
+	ptr_src[7]= 22;
+	ptr_src[8]= 9;
+	ptr_src[9]= 80;
+	ptr_src[10]= 5;
+	ptr_src[11]= 43;
+	ptr_src[12]= 2;
+	ptr_src[13]= 3;
+	ptr_src[14]= 1;
+	ptr_src[15]=0;
 	int i;
-	for(i=0;i<16;i+=1){
+/*	for(i=0;i<16;i+=1){
 		if (i<5) {
 			ptr_src[i]=2^i;
 		}
@@ -17,9 +34,11 @@ int main(){
 		else {
 			ptr_src[i]=2*i;
 		}
-	}
+	}*/
 	char * ptr_dst = malloc(16);
-	pm(ptr_src,ptr_dst);
+	apply_lineX(ptr_src,0);
+	printf("paso el apply \n");
+	save_mem(ptr_dst);
 	for(i=0;i<16;i+=1){
 		printf("elemento_%d es: %d\n",i,ptr_dst[i]);
 	}
