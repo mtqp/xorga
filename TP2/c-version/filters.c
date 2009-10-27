2c27a927cf416d664ef82faf59c16881ba03accb
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2009-10-27 00:40:44 +0000

    yeah frei yeah

---
+++ TP2/c-version/filters.c
@@ -0,0 +1,139 @@
+#include "filters.h"
+#include "math.h"
+
+const char OPERADOR_SOBEL_X[9] = {
+	-1, 0, 1,
+	-2, 0, 2,
+	-1, 0, 1
+};
+
+const char OPERADOR_SOBEL_Y[9] = {
+	-1, -2, -1,
+	 0,  0,  0,
+	 1,  2,  1
+};
+
+const char OPERADOR_ROBERTS_X[4] = {
+	1,  0,
+	0, -1
+};
+
+const char OPERADOR_ROBERTS_Y[4] = {
+	 0, 1,
+	-1, 0
+};
+
+const char OPERADOR_PREWITT_X[9] = {
+	-1, 0, 1,
+	-1, 0, 1,
+	-1, 0, 1
+};
+
+const char OPERADOR_PREWITT_Y[9] = {
+	-1, -1, -1,
+	 0,  0,  0,
+	 1,  1,  1
+};
+
+const float OPERADOR_FREICHEN_X[9] = {
+	-1.0,			0.0,	1.0,
+	-(sqrt(2.0)),	0.0,	sqrt(2.0),
+	-1.0,			0.0,	sqrt(2.0)
+};
+
+float OPERADOR_FREICHEN_Y[9] = {
+	-1.0,	-(sqrt(2.0)),	1.0,
+	 0.0,	  0.0,	     	0.0,
+	-1.0,	-(sqrt(2.0)),	1.0
+};
+
+/**
+ * cX - procesa la imagen con el operador X
+ *		src	array de pixels ordenados (grayscale)
+ *		dst	array de pixels final
+ *		width	ancho (en pixels) de la imagen
+ *		height	alto (en pixels) de la imagen
+ *		xorder	orden de la derivada x
+ *		yorder	orden de la derivada y
+ */
+void cFreiChen( const unsigned char* src, unsigned char *dst, int width, int height, int xorder, int yorder ) {
+	int x, y, k;
+
+	for( y = 1 ; y < height-1 ; y++ )
+		for( x = 1 ; x < width-1 ; x++ ) {
+			k=0;
+			if( xorder != 0 )
+				k =toCharS((unsigned char)(float_apply_mask( &src[width*(y-1)+x-1], width, OPERADOR_FREICHEN_X, 3 )));
+			if( yorder != 0 )
+				k+=toCharS((unsigned char)(float_apply_mask( &src[width*(y-1)+x-1], width, OPERADOR_FREICHEN_Y, 3 )));
+			dst[width*y+x]=toCharS(k);
+		}
+}
+void cSobel( const unsigned char* src, unsigned char *dst, int width, int height, int xorder, int yorder ) {
+	int x, y, k;
+	int line = lineSize(width,4);
+
+	for( y = 1 ; y < height-1 ; y++ )
+		for( x = 1 ; x < width-1 ; x++ ) {
+			k=0;
+			if( xorder != 0 )
+				k =toCharS(apply_mask( &src[line*(y-1)+x-1], line, OPERADOR_SOBEL_X, 3 ));
+			if( yorder != 0 )
+				k+=toCharS(apply_mask( &src[line*(y-1)+x-1], line, OPERADOR_SOBEL_Y, 3 ));
+			dst[line*y+x]=toCharS(k);
+		}
+}
+
+void cRoberts( const unsigned char* src, unsigned char *dst, int width, int height, int xorder, int yorder ) {
+	int x, y, k;
+	int line = lineSize(width,4);
+
+	for( y = 0 ; y < height-1 ; y++ )
+		for( x = 0 ; x < width-1 ; x++ ) {
+			k=0;
+			if( xorder != 0 )
+				k =toCharS(abs(apply_mask( &src[line*y+x], line, OPERADOR_ROBERTS_X, 2 )));
+			if( yorder != 0 )
+				k+=toCharS(abs(apply_mask( &src[line*y+x], line, OPERADOR_ROBERTS_Y, 2 )));
+			dst[line*y+x]=toCharS(k);
+		}
+}
+
+void cPrewitt( const unsigned char* src, unsigned char *dst, int width, int height, int xorder, int yorder ) {
+	int x, y, k;
+	int line = lineSize(width,4);
+
+	for( y = 1 ; y < height-1 ; y++ )
+		for( x = 1 ; x < width-1 ; x++ ) {
+			k=0;
+			if( xorder != 0 )
+				k =toCharS(abs(apply_mask( &src[line*(y-1)+x-1], line, OPERADOR_PREWITT_X, 3 )));
+			if( yorder != 0 )
+				k+=toCharS(abs(apply_mask( &src[line*(y-1)+x-1], line, OPERADOR_PREWITT_Y, 3 )));
+			dst[line*y+x]=toCharS(k);
+		}
+}
+
+/**
+ * apply_mask - aplica la máscara y devuelve el resultado
+ *		src		array de pixels ordenados (grayscale)
+ *		line		tamaño de línea del array (incluyendo el fill)
+ *		mask		máscara a aplicar
+ *		mask_sz		tamaño del lado de la máscara
+ */
+int apply_mask( const unsigned char* src, unsigned int line, const char* mask, unsigned int mask_sz ) {
+	int i,j,k=0;
+	for( i = 0 ; i < mask_sz ; i++ )
+		for( j = 0 ; j < mask_sz ; j++ )
+			k += mask[mask_sz*i+j] * src[line*i+j];
+	return k;
+}
+
+float float_apply_mask( const unsigned char* src, unsigned int line, float* mask, unsigned int mask_sz ) {
+	int i,j,k=0;
+	for( i = 0 ; i < mask_sz ; i++ )
+		for( j = 0 ; j < mask_sz ; j++ )
+			k += mask[mask_sz*i+j] * (float)(src[line*i+j]);
+	return k;
+}
+
---
+++ TP2/c-version/filters.h
@@ -0,0 +1,21 @@
+#ifndef __FILTERS_H__
+#define __FILTERS_H__
+
+/** toCharS - convierte x a char mediante saturación **/
+#define toCharS(x) x > 255? 255 : ( x < 0 ? 0 : x )
+
+/** lineSize - obtiene el tamaño de la línea incluyendo el alineado  **/
+#define lineSize(width,align) width-width%align+align*(width%align>0)
+
+/** abs - calcula el valor absoluto de una expresión **/
+#define abs(x) x>0?x:-x
+
+
+typedef void(FILTERFN)( const unsigned char* src, unsigned char* dst, int width, int height, int xorder, int yorder );
+FILTERFN cSobel;
+FILTERFN cRoberts;
+FILTERFN cPrewitt;
+FILTERFN cFreiChen;
+int apply_mask( const unsigned char* src, unsigned int line, const char *mask, unsigned int mask_sz );
+float float_apply_mask( const unsigned char* src, unsigned int line, float *mask, unsigned int mask_sz );
+#endif // __FILTERS_H__
