dd21097a1204c6f4647389741212336829511746
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-03-30 01:50:03 +0000

    ej3

---
+++ algo3/tp1/programadores.cpp
@@ -0,0 +1,71 @@
+#include<cmath>
+#include<fstream>
+#include<iostream>
+#include<string.h>
+#include<sstream>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define empezar_medicion(var) __asm__ __volatile__ ("rdtsc;mov %%eax, %0" : : "g" (var) );
+#define terminar_medicion(var) __asm__ __volatile__ ("rdtsc;sub %0,%%eax;mov %%eax,%0" : : "g" (var) );
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef unsigned long long int ullint;
+
+void print(const string* a, uint n){
+	for(uint i=0; i<n; i++) cout << a[i] << endl;
+}
+
+uint programadores_en_simultaneo(const string* ingresos,const string* egresos,uint n){
+	//cout << "Cantidad de programadores: " << n << endl;
+	//cout << "Ingresos: " << endl;
+	//print(ingresos,n);
+	//cout << "Egresos: " << endl;
+	//print(egresos,n);
+	int max=0,tmp=0,j=0,k=0;
+	while(j<n){
+		if(ingresos[j]<=egresos[k]){
+			tmp++;
+			j++;
+		}
+		else{
+			if(tmp>max) max=tmp;
+			tmp--;
+			k++;
+		}
+	}
+	if(tmp>max) max=tmp;
+	return max;
+}
+
+int main (int argc, char** argv){
+	ullint n;
+	ullint ts;
+	while(cin >> n && n!=-1){
+		string ingresos[n];
+		string egresos[n];
+		string delim;
+		//cout << "n: " << n << endl;
+		for(uint i=0; i<n; i++){ 
+			cin >> ingresos[i];
+			cin >> delim;
+			//cout << "ingreso: " << ingresos[i] << endl;
+		}
+		for(uint i=0; i<n; i++){
+			 cin >> egresos[i];
+		  	 cin >> delim;
+			 //cout << "egreso: " << egresos[i] << endl;
+		}
+		
+		if(argc>1 && string(argv[1])=="time"){
+			empezar_medicion(ts);
+			cout << programadores_en_simultaneo(ingresos,egresos,n);
+			cout << "\t\t[" << ts << "]" << endl;
+			terminar_medicion(ts);
+		}
+		else cout << programadores_en_simultaneo(ingresos,egresos,n) << endl;
+	}
+	return 0;
+}
