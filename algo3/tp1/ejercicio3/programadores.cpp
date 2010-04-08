363df54c91b40e2f7a1d9c62713276397223758e
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-08 04:37:41 +0000

    conteo de operaciones

--- algo3/tp1/ejercicio3/programadores.cpp
+++ algo3/tp1/ejercicio3/programadores.cpp
@@ -8,6 +8,8 @@
 
 #define empezar_medicion(var) __asm__ __volatile__ ("rdtsc;mov %%eax, %0" : : "g" (var) );
 #define terminar_medicion(var) __asm__ __volatile__ ("rdtsc;sub %0,%%eax;mov %%eax,%0" : : "g" (var) );
+#define O(n) contador+=(n)
+unsigned long long contador; //el contador de la cantidad de operaciones
 
 using namespace std;
 
@@ -19,21 +21,25 @@ void print(const string* a, uint n){
 }
 
 uint programadores_en_simultaneo(const string* ingresos,const string* egresos,uint n){
-	int max=0;	//max guarda la maxima cantidad de programadores en simultaneo hasta el momento
-	int tmp=0;	//tmp guarda la cantidad de programadores en simultaneo en un momento dado
-	int j=0,k=0;	//j se mueve dentro del array ingresos y k se mueve dentro del array de egresos
+	uint max=0;	//max guarda la maxima cantidad de programadores en simultaneo hasta el momento
+	uint tmp=0;	//tmp guarda la cantidad de programadores en simultaneo en un momento dado
+	uint j=0,k=0;	//j se mueve dentro del array ingresos y k se mueve dentro del array de egresos
+	O(4);
 	while(j<n){	//cuando veo todos los ingresos ya me alcanza para decidir
 		if(ingresos[j]<=egresos[k]){	//si ingresa alguien
 			tmp++;			//sumo un programador a los que tengo hasta el momento
 			j++;			//y paso al siguiente ingreso
+			O(6);
 		}
 		else{				//si egresa alguien
 			if(tmp>max) max=tmp;	//miro si la cantidad de programadores en simultaneo antes de que egrese ese programador es mayor a la maxima cantidad de programadores en simultaneo calculada anteriormente, si es asi actualizo max
 			tmp--;			//resto un programador a los que tengo hasta el momento
 			k++;			//y paso al siguiente egreso
+			O(7);
 		}
 	}
 	if(tmp>max) max=tmp;			//si es necesario actualizo el max
+	O(2);
 	return max;
 }
 
@@ -52,11 +58,16 @@ int main (int argc, char** argv){
 			 cin >> egresos[i];
 		  	 cin >> delim;
 		}
-		if(argc>1 && string(argv[1])=="time"){
+		if(argc>1 && string(argv[1])=="time"){	//si el argumento es "time", mido el tiempo
 			empezar_medicion(ts);
 			cout << programadores_en_simultaneo(ingresos,egresos,n);
 			cout << "\t\t[" << ts << "]" << endl;
 			terminar_medicion(ts);
+		}
+		else if(argc>1 && string(argv[1])=="count"){	//si el argumento es "count", cuento cantidad de operaciones
+			contador=0;
+			programadores_en_simultaneo(ingresos,egresos,n);
+			cout << contador << endl; //imprimo la cuenta
 		}
 		else cout << programadores_en_simultaneo(ingresos,egresos,n) << endl;
 	}
