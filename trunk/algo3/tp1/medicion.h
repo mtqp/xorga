44de2784b551d6baf9f6f91b243928efefa667bc
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-09 14:20:54 +0000

    [Tp1] Agregado medicion.h

---
+++ trunk/algo3/tp1/medicion.h
@@ -0,0 +1,28 @@
+#ifndef __MEDICION_H__
+#define __MEDICION_H__
+
+#define empezar_medicion(var) __asm__ __volatile__ ("rdtsc;mov %%eax, %0" : : "g" (var) )
+#define terminar_medicion(var) __asm__ __volatile__ ("rdtsc;sub %0,%%eax;mov %%eax,%0" : : "g" (var) )
+	
+/*
+#include<sys/time.h>
+
+#define empezar_medicion(var) \
+	gettimeofday(&tiempo, NULL);  \
+	var = tiempo.tv_usec
+
+#define terminar_medicion(var) \
+	gettimeofday(&tiempo, NULL); \
+	var = tiempo.tv_usec - var
+
+timeval tiempo;
+*/
+
+#define O(n) contador+=(n)
+#define reiniciar_contador() contador=0
+
+/* contador de la cantidad de operaciones */
+unsigned long long contador;
+
+#endif
+
