01630a3db73c1647dc2dc37386402e2ef26607af
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-09 14:16:09 +0000

    [Tp1] Código iterativo ejercicio1 + generador de tests

--- trunk/algo3/tp1/ejercicio1/bn_mod_n.cpp
+++ trunk/algo3/tp1/ejercicio1/bn_mod_n.cpp
@@ -1,68 +1,70 @@
-#include<iostream>
-#include<sys/time.h>
+#include <iostream>
+#include <iomanip>
+#include "../medicion.h"
+
 using namespace std;
-
-#define empezar_medicion(tvar, var) \
-	gettimeofday(&tvar, NULL);  \
-	var = tvar.tv_usec
-
-#define terminar_medicion(tvar, var) \
-	gettimeofday(&tvar, NULL); \
-	var = tvar.tv_usec - var
 
 typedef unsigned int uint;
 typedef unsigned long long int ullint;
 
-uint bn_mod_n( uint b, ullint n )
+ullint operaciones = 0;
+
+ullint bn_mod_n( uint b, ullint n )
 {
+	reiniciar_contador();
+
 	uint c = b%n;
+	ullint tmp=1;
+	ullint m = n;
 
-	// caso base 1: b == 1 => resultado = 1
-	if( b == 1 )
-		return 1;
-	
-	// caso base 2: n == 1 => X mod 1 = 0
-	if( n == 1 )
+	// caso base 1: n == 1 => X mod 1 = 0
+	// caso base 2: c == 0 => 0 mod X = 0
+	O(6);
+	if( n == 1 || c == 0 ) 
 		return 0;
-	
-	ullint m   = n%2;
-	ullint csq = c*c;
-	ullint tmp;
-	if( m == 1 )
-		tmp = c;
-	else
-		tmp = 1;
 
-	while( m < n )
+	// caso base 3: c == 1 => resultado = 1
+	O(1);
+	if( c == 1 )
+		return 1;
+
+	while( m > 0 )
 	{
-		tmp *= csq;
-		tmp %= n;
-		m += 2;
-	}
+		if( m%2 == 1 )
+		{
+			tmp *= c;
+			tmp %= n;
+			O(2);
+		}
+		m /= 2;
+		c *= c;
+		c %= n;
+		O(6);
+	}	
 
 	return tmp;
 }
-
-#include<iomanip>
 
 int main (int argc, char** argv)
 {
 	uint b, r;
 	ullint n;
-	ullint ts;
-	timeval tv;
-
-	while(cin >> b && b!=-1){
+	uint ts=0;
+	
+	while(cin >> b && b!=(uint)-1){
 		cin >> n;
 		if(argc>1 && string(argv[1])=="time"){
-			empezar_medicion(tv,ts);
+
+			empezar_medicion(ts);
 			r = bn_mod_n(b,n);
-			terminar_medicion(tv,ts);
+			terminar_medicion(ts);
+
 			cout << setw(4) << b << "^" << setw(4) << n << " mod " << setw(4) << n << " = " << setw(4) << r;
-			cout << "\t[" << ts << "µs]" << endl;
+			cout << "\t[" << setw(2) << ts << "c," << setw(3) << contador << "ops]" << endl;
 		}
 		else cout << bn_mod_n(b,n) << endl;
 	}
+	
 	return 0;
 }
 
