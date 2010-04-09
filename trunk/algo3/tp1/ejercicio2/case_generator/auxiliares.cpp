d339eed1d2bda604650526f498bf992113e13400
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-09 14:09:37 +0000

    funciona perfectamente el generador de rondas, de todas formas prueben varios caso a mano a ver si alguno no da! entre hoy y mañana voy a tener el generador de NO_rondas, es ligeramente mas complejo, pero se hiper puede. abrazos mis amigos!

--- trunk/algo3/tp1/ejercicio2/case_generator/auxiliares.cpp
+++ trunk/algo3/tp1/ejercicio2/case_generator/auxiliares.cpp
@@ -30,20 +30,10 @@ bool shuffle_completo(bool *a_bools, int size){
 }
 
 void agregar_ronda(list<int> *array_amigas, int *orden, int cant){
-	cout << "agregar ronda linea 33" << endl;		
-	/*int i=0;
-	cout << endl << "arreglo ORDEN == ";
-	for(i;i<cant;i++){
-		cout << orden[i] << " ";
-	}
-	cout << endl << endl;
-	*/
 	int i = 1;
 	array_amigas[orden[0]].push_back(orden[cant-1]);		//agrego las relaciones a izquierda
-	cout << "agregar ronda linea 36" << endl;		
 	for(i;i<cant;i++){
 		array_amigas[orden[i]].push_back(orden[i-1]);
-		cout << "agregar ronda linea 39 con i==" << i << endl;		
 	}//convencerse q nunca agregar en amigas[i] efectivamente a i;
 
 	i=0;
@@ -51,15 +41,12 @@ void agregar_ronda(list<int> *array_amigas, int *orden, int cant){
 	for(i;i<cant-1;i++){
 		array_amigas[orden[i]].push_back(orden[i+1]);
 	}
-	cout << "agregar ronda linea 41" << endl;		
 }
 
 bool no_repita_amigas(list<int> lista,/* int size,*/ int am_rand){
-	//cout << "no repita amigas linea 52" << endl;
 	list<int> :: iterator it;
 	bool res = true;
 
-	//cout << "no repita amigas linea 57" << endl;
 	for ( it=lista.begin();it!=lista.end(); it++ ){
 		res = res && !(*it==am_rand);
 	}
@@ -67,7 +54,7 @@ bool no_repita_amigas(list<int> lista,/* int size,*/ int am_rand){
 }
 
 void ronda_posible(list<int> *array_amigas, int cant_amigas){
-	cout << endl << "funcion ronda posible en aux.cpp linea 56" << endl;
+
 	bool generar_ronda[cant_amigas];
 	int ronda_amigas[cant_amigas];
 	int amigas[cant_amigas];
@@ -76,16 +63,14 @@ void ronda_posible(list<int> *array_amigas, int cant_amigas){
 	int index_vacio;
 	
 	srand(time(NULL));
-	cout << "ronda posible linea 65" << endl;	
+
 	for(i=0;i<cant_amigas;i++){
 		generar_ronda[i] = false;
 		amigas[i] = i;
 	}
 	i = 0;
-//	cout << "sera cant amigas random??? == " << cant_amigas << endl;
-	cout << "ronda posible linea 81 " << endl;	
+
 	while(!shuffle_completo(generar_ronda,cant_amigas) || (i!=cant_amigas)){
-//		cout << "ronda posible linea 83 con i==" << i << endl;	
 		index_vacio = rand()%cant_amigas;
 		if(!generar_ronda[index_vacio]){
 			generar_ronda[index_vacio] = true;
@@ -93,25 +78,25 @@ void ronda_posible(list<int> *array_amigas, int cant_amigas){
 			i++;
 		}
 	}
-	cout << "ronda posible linea 91" << endl;	
 	////////recien aca tengo en ronda_amigas una solucion ahora tengo q completar con mas relaciones
 	///x el momento agrego la ronda posible como ultimo nodo de array amigas podria despues hacerse un rand 
 	/// entre agregar adelante y agregar atras, no es tan dificil... usar un if mod2
 	agregar_ronda(array_amigas, ronda_amigas, cant_amigas);
-	cout << "ronda posible linea 98" << endl;		
 	int cant_am_rand;
 	int am_rand;
 	int am_camino;
+	bool puede_mas_amigas;
+
 	for(i=0;i<cant_amigas;i++){
-		cout << "ronda posible linea 103 con i==" << i << endl;
 		cant_am_rand = rand()%cant_amigas-2;				//no puede ser amiga de si misma
 		j=0;												//ni de las q ya es xq es camino
-		cout << "ronda posible linea 106 cant amigas random == " << cant_am_rand << endl;
-		while(j<cant_am_rand){
-			cout << "ronda posible linea 108 j==" << j << endl;		
+		puede_mas_amigas = true;
+
+		while((j<cant_am_rand) && puede_mas_amigas){
 			am_rand = rand()%cant_amigas;
 			if (am_rand != i){
-				if (no_repita_amigas(array_amigas[i],/*cant_amigas,*/am_rand)){	//q no se quiera hacer amiga de si misma o de la q ya era amiga
+				puede_mas_amigas = (array_amigas[i].size()<cant_amigas-1);
+				if (no_repita_amigas(array_amigas[i],am_rand)){	//q no se quiera hacer amiga de si misma o de la q ya era amiga
 					if (rand()%2){
 						array_amigas[i].push_front(am_rand);
 						array_amigas[am_rand].push_front(i);
@@ -124,6 +109,5 @@ void ronda_posible(list<int> *array_amigas, int cant_amigas){
 			}
 		}
 	}
-	cout << "fin ronda posible linea 103" << endl;
 }
 
