e9d23fd0720ce8bc9507a2b834f4e296f867855c
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-09 03:29:39 +0000

    a veces se cuelga, todavia no da resultados buenos

--- trunk/algo3/tp1/ejercicio2/case_generator/auxiliares.cpp
+++ trunk/algo3/tp1/ejercicio2/case_generator/auxiliares.cpp
@@ -30,29 +30,54 @@ bool shuffle_completo(bool *a_bools, int size){
 }
 
 void agregar_ronda(list<int> *array_amigas, int *orden, int cant){
+	cout << "agregar ronda linea 33" << endl;		
+	/*int i=0;
+	cout << endl << "arreglo ORDEN == ";
+	for(i;i<cant;i++){
+		cout << orden[i] << " ";
+	}
+	cout << endl << endl;
+	*/
 	int i = 1;
 	array_amigas[orden[0]].push_back(orden[cant-1]);
+	cout << "agregar ronda linea 36" << endl;		
 	for(i;i<cant;i++){
 		array_amigas[orden[i]].push_back(orden[i-1]);
+		cout << "agregar ronda linea 39 con i==" << i << endl;		
 	}//convencerse q nunca agregar en amigas[i] efectivamente a i;
+	cout << "agregar ronda linea 41" << endl;		
 }
 
-bool no_repita_amigas(list<int> lista, int iesimo, int size){
+bool no_repita_amigas(list<int> lista, int size){
+	//cout << "no repita amigas linea 52" << endl;
 	list<int> :: iterator it;
 	int cant_veces[size];
 	bool no_repite = true;
-	int z = 0;
+	
+	//inicializa el array
+	for(int d=0;d<size;d++){
+		cant_veces[d] = 0;
+	}
+	
+	//cout << "no repita amigas linea 57" << endl;
 	for ( it=lista.begin();it!=lista.end(); it++ ){
 		cant_veces[*it]++;
 	}
-
+	
+	int z = 0;
+	//cout << "no repita amigas linea 62" << endl;
 	while(z<size && no_repite){
+		//cout << "valor bool no repite == " << no_repite << endl;
 		no_repite = cant_veces[z]<2;
+		z++;
 	}
-	return no_repite && (cant_veces[iesimo]==0);
+	bool res = no_repite;
+//	cout << "no repita amigas resultado == " << res << endl;
+	return res;
 }
 
 void ronda_posible(list<int> *array_amigas, int cant_amigas){
+	cout << endl << "funcion ronda posible en aux.cpp linea 56" << endl;
 	bool generar_ronda[cant_amigas];
 	int ronda_amigas[cant_amigas];
 	int amigas[cant_amigas];
@@ -61,13 +86,16 @@ void ronda_posible(list<int> *array_amigas, int cant_amigas){
 	int index_vacio;
 	
 	srand(time(NULL));
-		
+	cout << "ronda posible linea 65" << endl;	
 	for(i=0;i<cant_amigas;i++){
 		generar_ronda[i] = false;
 		amigas[i] = i;
 	}
 	i = 0;
-	while(!shuffle_completo(generar_ronda,cant_amigas)){
+//	cout << "sera cant amigas random??? == " << cant_amigas << endl;
+	cout << "ronda posible linea 81 " << endl;	
+	while(!shuffle_completo(generar_ronda,cant_amigas) || (i!=cant_amigas)){
+//		cout << "ronda posible linea 83 con i==" << i << endl;	
 		index_vacio = rand()%cant_amigas;
 		if(!generar_ronda[index_vacio]){
 			generar_ronda[index_vacio] = true;
@@ -75,29 +103,35 @@ void ronda_posible(list<int> *array_amigas, int cant_amigas){
 			i++;
 		}
 	}
+	cout << "ronda posible linea 91" << endl;	
 	////////recien aca tengo en ronda_amigas una solucion ahora tengo q completar con mas relaciones
 	///x el momento agrego la ronda posible como ultimo nodo de array amigas podria despues hacerse un rand 
 	/// entre agregar adelante y agregar atras, no es tan dificil... usar un if mod2
 	agregar_ronda(array_amigas, ronda_amigas, cant_amigas);
-	
+	cout << "ronda posible linea 98" << endl;		
 	int cant_am_rand;
 	int am_rand;
 	int am_camino;
 	for(i=0;i<cant_amigas;i++){
+		cout << "ronda posible linea 103 con i==" << i << endl;
 		cant_am_rand = rand()%cant_amigas;				//no puede ser amiga de si misma
 		j=0;
+		cout << "ronda posible linea 106 cant amigas random == " << cant_am_rand << endl;
 		while(j<cant_am_rand){
-			am_rand = rand()%cant_amigas;
-			am_rand--;
-			if (no_repita_amigas(array_amigas[i],i,cant_amigas)){	//q no se quiera hacer amiga de si misma o de la q ya era amiga
-				if (rand()%2){
-					array_amigas[i].push_front(am_rand);
-				} else {
-					array_amigas[i].push_back(am_rand);
+			cout << "ronda posible linea 108 j==" << j << endl;		
+			am_rand = rand()%cant_amigas+1;
+			if (am_rand != i){
+				if (no_repita_amigas(array_amigas[i],cant_amigas)){	//q no se quiera hacer amiga de si misma o de la q ya era amiga
+					if (rand()%2){
+						array_amigas[i].push_front(am_rand);
+					} else {
+						array_amigas[i].push_back(am_rand);
+					}
+					j++;
 				}
-				j++;
 			}
 		}
 	}
+	cout << "fin ronda posible linea 103" << endl;
 }
 
