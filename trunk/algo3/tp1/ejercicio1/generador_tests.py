c40a9e8cf23cf6561037e1fd6e1bf69ac4b738b9
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-14 05:52:17 +0000

    [Tp1] reparaciones generador ej1

--- trunk/algo3/tp1/ejercicio1/generador_tests.py
+++ trunk/algo3/tp1/ejercicio1/generador_tests.py
@@ -1,24 +1,27 @@
+#!/usr/bin/python
 # Generador de archivos de test random para el ejercicio 1
+from random import seed, randint
 
-import random
+def main():
+	seed()
 
-random.seed()
+	cantidad = 100
+	maximo_b = 200
+	maximo_n = 1000
 
-cantidad = 500
-maximo_b = 200
-maximo_n = 10000000
+	fIn  = open('test.in', 'w')
+	fOut = open('test.out','w')
 
-fIn  = open('test.in', 'w')
-#fOut = open('test.out','w')
+	for i in range(cantidad):
+		b = randint(0,maximo_b)
+		n = randint(1,maximo_n)
+		r = (b**n)%n
+		fIn.write('%s %s\n'%(b,n))
+		fOut.write('%s\n'%r)
 
-for i in range(cantidad):
-	b = random.randint(0,maximo_b)
-	n = random.randint(1,maximo_n)
-	#r = (b**n)%n
-	fIn.write('%s %s\n'%(b,n))
-	#fOut.write('%s\n'%r)
+	fIn.write('-1 -1')
+	fIn.close()
+	fOut.close()
 
-fIn.write('-1 -1')
-fIn.close()
-#fOut.close()
-
+if __name__=="__main__":
+	main()
