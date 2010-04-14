c0cd80989a19a685bfc3a5553050b3b9ef03d13e
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-14 18:30:40 +0000

    [Tp1] Ej1-generador de test parsea

--- trunk/algo3/tp1/entrega/Ej1/generador_tests.py
+++ trunk/algo3/tp1/entrega/Ej1/generador_tests.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python
 # Generador de archivos de test random para el ejercicio 1
-from random import seed, randint
 
-def main():
+from optparse import OptionParser
+
+from random import seed, randint
+import sys
+
+def main(options):
 	seed()
 
-	cantidad = 100
-	maximo_b = 200
-	maximo_n = 1000
+	cantidad = options.count
+	maximo_b = options.b
+	maximo_n = options.n
+	nombre   = options.archivo
 
-	fIn  = open('test.in', 'w')
-	fOut = open('test.out','w')
+	fIn  = open(nombre+'.in' ,'w')
+
+	if options.out:	
+		fOut = open(nombre+'.out','w')
 
 	for i in range(cantidad):
 		b = randint(0,maximo_b)
 		n = randint(1,maximo_n)
-		r = (b**n)%n
 		fIn.write('%s %s\n'%(b,n))
-		fOut.write('%s\n'%r)
+		if options.out: 
+			r = (b**n)%n
+			fOut.write('%s\n'%r)
 
 	fIn.write('-1 -1')
 	fIn.close()
-	fOut.close()
+	if options.out: 
+		fOut.close()
 
 if __name__=="__main__":
-	main()
+	parser = OptionParser()
+	parser.add_option("-a", "--archivo", dest="archivo", help="escribe los test en ARCHIVO.in [ARCHIVO.out]", metavar='ARCHIVO', default='test')
+	parser.add_option("-o", "--out", dest="out", help="escribe el resultado del test", action='store_true', default=False)
+	parser.add_option("-c", "--count", dest="count", help="genera COUNT casos de prueba", type='int', default=1000, metavar='COUNT')
+	parser.add_option("-b", "--max_b", dest="b", help="establece el rango de b desde 0 hasta B", type='int', default=200, metavar='B')
+	parser.add_option("-n", "--max_n", dest="n", help="establece el rango de n desde 1 hasta N", type='int', default=100000, metavar='N')
+	(options, args) = parser.parse_args()
+	main(options)
