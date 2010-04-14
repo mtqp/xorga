e772cbd8ee3c035732ec49560fbf16ef2684e7d8
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-14 19:29:54 +0000

    [Tp1] creo que terminado de una vez

--- trunk/algo3/tp1/entrega/Ej3/generador_casos.py
+++ trunk/algo3/tp1/entrega/Ej3/generador_casos.py
@@ -1,50 +1,62 @@
 #!/usr/bin/python
+# Generador de casos random para el ejercicio 3
 
+from optparse import OptionParser
 from random import randint, seed
 
-casos = 1000
-max_programadores = 100
+def main(options):
+	casos = options.count
+	max_programadores = options.n
 
-seed()
+	seed()
 
-fIn = open('test.in','w')
+	fIn = open(options.archivo+'.in','w')
 
-ingresos = []
-egresos = []
-
-for i in range(casos):
-	programadores = randint(0,max_programadores-1)
-	caso = randint(0,1)
-	for j in range(programadores-caso):
-		horas = randint(0,23)
-		minutos = randint(0,58)
-		segundos = randint(0,58)
-		ingresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,j+1)]
-		
-		horas_e = randint(horas,23)
-		if horas_e == horas:
-			minutos_e = randint(minutos,59)
-		else:
-			minutos_e = randint(0,59)
-		if minutos_e == minutos:
-			segundos_e = randint(segundos,59)
-		else:
-			segundos_e = randint(0,59)
-		egresos += ["%.2d:%.2d:%.2d %s"%(horas_e,minutos_e,segundos_e,j+1)]
-
-	if caso == 1:
-		ingresos += ["23:59:58 %s"%(programadores)]
-		egresos += ["23:59:59 %s"%(programadores)]
-		
-	ingresos=sorted(ingresos)
-	egresos=sorted(egresos)
-	
-	fIn.write("%s\n"%(programadores))
-	for j in range(programadores):
-		fIn.write("%s\n"%(ingresos[j]))
-	
-	for j in range(programadores):
-		fIn.write("%s\n"%(egresos[j]))
 	ingresos = []
 	egresos = []
-fIn.write("-1")
+
+	for i in range(casos):
+		programadores = randint(0,max_programadores-1)
+		caso = randint(0,1)
+		for j in range(programadores-caso):
+			horas = randint(0,23)
+			minutos = randint(0,58)
+			segundos = randint(0,58)
+			ingresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,j+1)]
+		
+			horas_e = randint(horas,23)
+			if horas_e == horas:
+				minutos_e = randint(minutos,59)
+			else:
+				minutos_e = randint(0,59)
+			if minutos_e == minutos:
+				segundos_e = randint(segundos,59)
+			else:
+				segundos_e = randint(0,59)
+			egresos += ["%.2d:%.2d:%.2d %s"%(horas_e,minutos_e,segundos_e,j+1)]
+
+		if caso == 1:
+			ingresos += ["23:59:58 %s"%(programadores)]
+			egresos += ["23:59:59 %s"%(programadores)]
+		
+		ingresos=sorted(ingresos)
+		egresos=sorted(egresos)
+	
+		fIn.write("%s\n"%(programadores))
+		for j in range(programadores):
+			fIn.write("%s\n"%(ingresos[j]))
+	
+		for j in range(programadores):
+			fIn.write("%s\n"%(egresos[j]))
+		ingresos = []
+		egresos = []
+	fIn.write("-1")
+
+if __name__=="__main__":
+	parser = OptionParser()
+	parser.add_option("-a", "--archivo", dest="archivo", help="escribe los test en ARCHIVO.in [ARCHIVO.out]", metavar='ARCHIVO', default='test')
+	parser.add_option("-c", "--casos", dest="count", help="genera COUNT casos de prueba", type='int', default=1000, metavar='COUNT')
+	parser.add_option("-n", "--programadores", dest="n", help="establece la cantidad maxima de programadores por caso", type='int', default=100, metavar='N')
+	(options, args) = parser.parse_args()
+	main(options)
+
