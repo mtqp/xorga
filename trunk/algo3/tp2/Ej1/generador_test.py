2a7fb77db74a65ef02b49f1424d9d2349eaf1e47
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-16 05:03:59 +0000

    me anime con el python :p

---
+++ trunk/algo3/tp2/Ej1/generador_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# Generador de archivos de test random para el ejercicio 1
+from random import seed, randint
+
+def main():
+	seed()
+
+	cantidad = 15
+	maximo_n = 20
+	maximo_elem = 20
+	secuencia = []	
+	
+	fIn  = open('test.in', 'w')
+
+	for i in range(cantidad):
+		n = randint(1,maximo_n)
+		fIn.write('%s   '%(n))
+		for j in range(n):
+			elem = randint(-maximo_elem,maximo_elem)
+			secuencia += [elem]
+			fIn.write('%s '%(secuencia[j]))
+		fIn.write('\n')
+
+	fIn.write('-1')
+	fIn.close()
+
+if __name__=="__main__":
+	main()
