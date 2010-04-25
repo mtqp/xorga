4c310c0ef623b66c92cf281cd1c295c2ca5cf6fc
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-25 23:45:57 +0000

    todavia esta en proceso

---
+++ trunk/algo3/tp2/Ej2/n_3_ciudad.h
@@ -0,0 +1,28 @@
+typedef unsigned int uint;
+typedef unsigned long long int ullint;
+
+typedef struct _Nodo_lista{
+	/*
+		invariante de arista: pi1 < pi2 SIEMPRE
+	*/
+	uint _pi1;
+	uint _pi2;
+	Nodo_lista* _siguiente_lista;
+	Nodo_lista* _anterior_lista;
+}Nodo_lista;
+
+typedef struct _Lista {
+	Nodo_lista * _primero_lista;
+	Nodo_lista * _ultimo_lista;
+}Lista;
+
+typedef struct _Nodo_matriz {
+	/*
+		invariante de nodo matriz: 
+			if(_adyacente) 	ptr_arista != NULL
+			else 			ptr_arista == NULL
+	*/
+	int _adyacente;				// es 1 o 0, tipo bool
+	Nodo_lista *_ptr_arista;	
+}Nodo_matriz;
+
