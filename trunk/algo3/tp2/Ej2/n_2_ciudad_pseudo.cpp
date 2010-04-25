7db0c314b17fc0aa2de1fedb38ad3c5c50f94130
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-04-25 20:00:49 +0000

    si baby

--- trunk/algo3/tp2/Ej2/n_2_ciudad_pseudo.cpp
+++ trunk/algo3/tp2/Ej2/n_2_ciudad_pseudo.cpp
@@ -1,91 +1,63 @@
 #include<cmath>
 #include<iostream>
-#include<stack>
+#include<queue>
 #include "../medicion.h"
-#define pila stack<uint>
+#define no_fuertemente_conexo 	false
+#define fuertemente_conexo		true 
 
 using namespace std;
 
 typedef unsigned int uint;
 typedef unsigned long long int ullint;
 
-/////////////////////////////////////////////////////////////
-//////////////////////PSEUDO-CODE////////////////////////////
-/////////////////////////////////////////////////////////////
+///////////////////////AUXILIARES////////////////////////////
 
-
-/*termino siendo DFS creo.*/
-
-bool ciudad(uint** matriz_adyacencia, uint tam_matriz){
-	/*
-	necesito:
-		- la matriz de adyacencia
-		- la copia de la matriz de adyacencia (booleana) pero en vez de decir si hay una arista entre (u,v), 
-			dice si esa arista ya forma parte de un camino simple.
-		- un array de bools de los nodos, para cuando se busque cada camino se chequee q no estes loopeando.
-	*/
-
-	case: [una arista/dos nodos] = return no_fuertemente_conexo
-	case: [algun vertice suelto] = return no_fuertemente_conexo
-	-------------------------------------------------------------
-	sino ---->
-	------------------------------------------------------------
-	bool matriz_aristas_caminos [tam_matriz][tam_matriz];
-	setear_false_toda(matriz_aristas_caminos, tam_matriz); 
-	
-	bool nodos_marcados[n];
-	setear_false_todo(nodos_marcados,n);
-	
-	cola c; //(no se si la cola era, tiene q ser FIFO)
-	int M = cantidad_aristas(matriz_adyacencia, tam_matriz)/2;
-
-	while(M>0){
-		//siempre entra aca con una estr FIFO vacia (si se llama cola mejor jajaj)
-		int primer_push = dame_nodo_camino_no_visto(DE_ALGUN_LADO);
-		c.push(primer_push);
-		bool con_caminos = true;
-	
-		int nodo_camino = primer_push;
-		nodos_marcados[nodo_camino] = true;
-	
-		bool es_camino_simple;
-	
-		while(con_caminos = tengaAlgunAdyacente_y_noEsXDondeVenia(nodo_camino,nodos_marcados,int sig_adyacente)){
-					
-			if (es_camino_simple = (sig_adyacente == primer_push))	//listo busco siguentes caminos
-				break;
-		
-			nodos_marcados[sig_adyacente] = true;					//sino, sigo encolando el posible camino
-			c.push(sig_adyacente);
-		}
-
-		if (!con_caminos) return no_fuertemente_conexo	//ya esta, alguno no puede lograr camino
-		else {
-			if(es_camino_simple){
-				while(!c.empty()){					//vacio la estr FIFO
-					uint popeado = c.front();
-					c.pop();
-					matriz_aristas_caminos[popeado][c.front()] = true;
-					matriz_aristas_caminos[c.front()][popeado] = true;
-					nodo_marcado[popeado] = false;	//reseteo el marcados para volver a buscar caminos
-					M--;							//decremento M = M-aristasDeLCamino
-				}
-			}
-		}
-	}
-	//si sale aca es xq paso x todas las aristas y siempre encontro camino para volver
-	return es_fuertemente_conexo;
-}
-////////////////////////////////////////////
-
-int cantidad_aristas(matriz M, int n){
+bool una_arista_dos_nodos(uint** matriz_adyacencia, uint n){
+	//si devuelve true ==> no es FC
+	/**costo de esta funcion, O(n²), desde la funcion ciudad se llama UNA sola vez**/
 	int res = 0;
-	for (i in range n)
-		for(j i range n)
-			res += M[i][j];
+	for (int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			res += matriz_adyacencia[i][j];
+	return (res == 2);
 }
 
-dame_nodo_camino_no_visto(DE_ALGUN_LADO){
+bool algun_vertice_suelto(uint** matriz_adyacencia, uint n){
+	//si devuelve true ==> no es FC
+	/**costo de esta funcion, O(n²), desde la funcion ciudad se llama UNA sola vez**/
+	int cant_adyacencias;
+	for (int i=0;i<n;i++){
+		cant_adyancencias = 0;
+		for(int j=0;j<n;j++){
+			cant_adyacencias += matriz_adyacencia[i][j];
+		}
+		if (cant_adyacencias == 0) return true;
+	}
+	return false;
+}
+
+void setear_matriz_false(bool** matriz_aristas_caminos, uint n, uint m){
+	//costo == O(n²) (se realiza una sola vez)
+	for (int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			matriz_aristas_caminos[i][j] = false;
+}
+
+void setear_array_false(bool* nodos_marcados, uint n){
+	//costo == O(n) (se realiza una sola vez)
+	for (int i=0;i<n;i++)
+		nodos_marcados[i] = false;
+}
+
+int cantidad_aristas(bool** M, int n, int m){		//costo, O(n*m) = O(n²)
+	int res = 0;
+	for (int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			res += M[i][j];
+	return res;
+}
+
+uint dame_nodo_camino_no_visto(DE_ALGUN_LADO){
 	/*
 	estaba pensando q podria tener una lista de los vertices q use q irlos borrando
 	a medida q encuentro camino, entonces dame_nodo_camino_no_visto, te tira el 1er camino de la lista
@@ -106,10 +78,81 @@ bool tengaAlgunAdyacente_y_noEsXDondeVenia(nodo_camino,(array de) nodos_marcados
 //tengo la sensacion q es n², de todas formas chequeenlo antes q me siente a escribirlo bien
 //hay casos q no contemplo? cualquier ayuda modificacion es bienvenida, buen finde!
 
+
 /////////////////////////////////////////////////////////////
-////////////////////FIN-PSEUDO-CODE//////////////////////////
+//////////////////////////FUNCION////////////////////////////
 /////////////////////////////////////////////////////////////
 
+/*termino siendo DFS.*/
+
+bool ciudad(uint** matriz_adyacencia, uint tam_matriz){
+	/*
+	necesito:
+		- la matriz de adyacencia
+		- la copia de la matriz de adyacencia (booleana) pero en vez de decir si hay una arista entre (u,v), 
+			dice si esa arista ya forma parte de un camino simple.
+		- un array de bools de los nodos, para cuando se busque cada camino se chequee q no estes loopeando.
+	*/
+
+	if(una_arista_dos_nodos(matriz_adyacencia, tam_matriz)) = return no_fuertemente_conexo;
+	if(algun vertice suelto(matriz_adyacencia, tam_matriz)) = return no_fuertemente_conexo;
+	
+	/*-------------------------------------------------------------
+							si no es un caso base ---->
+	------------------------------------------------------------*/
+	bool matriz_aristas_caminos [tam_matriz][tam_matriz];
+	setear_matriz_false(matriz_aristas_caminos, tam_matriz, tam_matriz); 
+	
+	bool nodos_marcados[n];
+	setear_array_false(nodos_marcados,n);
+	
+	queue<uint> q; 								//estr FIFO
+	int M = cantidad_aristas(matriz_adyacencia, tam_matriz, tam_matriz)/2;
+
+	bool con_caminos;
+	bool es_camino_simple;
+	uint nodo_camino;
+	uint primer_push;
+	uint sig_adyacente;
+
+	while(M>0){
+		//q empty
+		primer_push = dame_nodo_camino_no_visto(DE_ALGUN_LADO);
+		nodo_camino = primer_push;
+		q.push(primer_push);
+		
+		nodos_marcados[nodo_camino] = true;
+		//con_caminos = true;
+	
+		while(con_caminos = tengaAlgunAdyacente_y_noEsXDondeVenia(nodo_camino,ALGUN PARAM MAS CAPAS, nodos_marcados, &sig_adyacente)){
+					
+			if (es_camino_simple = (sig_adyacente == primer_push))	//listo busco siguentes caminos
+				break;
+		
+			nodos_marcados[sig_adyacente] = true;					//sino, sigo encolando el posible camino
+			q.push(sig_adyacente);
+		}
+
+		if (!con_caminos) return no_fuertemente_conexo;	//ya esta, alguno no puede lograr camino
+		else {
+			if(es_camino_simple){
+				while(!q.empty()){					//vacio la estr FIFO
+					uint popeado = q.front();
+					q.pop();
+					matriz_aristas_caminos[popeado][q.front()] = true;
+					matriz_aristas_caminos[q.front()][popeado] = true;
+					nodo_marcado[popeado] = false;	//reseteo el marcados para volver a buscar caminos
+					M--;							//decremento M = M-aristasDeLCamino
+				}
+			}
+		}
+	}
+	//si sale aca es xq paso x todas las aristas y siempre encontro camino para volver
+	return fuertemente_conexo;
+}
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
 void print_matriz(uint** M, uint m, uint dos){
 	for(uint i=0; i<m; i++){
