518e8b53e65c10b8e9056e8e7e29aea94883d27f
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-05-02 23:54:18 +0000

    generador 3 y cant op 3

--- trunk/algo3/tp2/Ej3/case_generator.py
+++ trunk/algo3/tp2/Ej3/case_generator.py
@@ -2,30 +2,30 @@
 
 from random import randint, seed, choice
 
-casos = 6
-max_esquinas = 7
+casos = 1000
+max_habitaciones = 100
 
 seed()
 
 fIn = open('test.in','w')
 
 for i in range(casos):
-	esquinas = randint(4,max_esquinas)
-	max_calles = (esquinas*(esquinas-1))/2
-	calles = randint(0,max_calles)
-	llaves = (esquinas-2)/2
-	if (llaves<0): llaves=0
-	fIn.write("%s %s %s\n"%(esquinas, llaves, calles))
-	habitaciones = [x for x in range(2,esquinas)]
+	habitaciones = randint(4,max_habitaciones)
+	max_pasillos = (habitaciones*(habitaciones-1))/2
+	pasillos = randint(0,max_pasillos)
+	llaves = randint(0,(habitaciones-2)/2)
+	#if (llaves<0): llaves=0
+	fIn.write("%s %s %s\n"%(habitaciones, llaves, pasillos))
+	h = [x for x in range(2,habitaciones)]
 	for j in range(llaves):
-		esta = choice(habitaciones)
-		habitaciones.remove(esta)
-		abre = choice(habitaciones)
-		habitaciones.remove(abre)
+		esta = choice(h)
+		h.remove(esta)
+		abre = choice(h)
+		h.remove(abre)
 		fIn.write("  %s %s\n"%(esta, abre))
-	extremos = [x+1 for x in range(esquinas)]
-	candidatos_a_extremos = [[y for y in extremos if y!=x+1] for x in range(esquinas)]
-	for j in range(calles):
+	extremos = [x+1 for x in range(habitaciones)]
+	candidatos_a_extremos = [[y for y in extremos if y!=x+1] for x in range(habitaciones)]
+	for j in range(pasillos):
 		extremo1 = choice(extremos)
 		extremo2 = choice(candidatos_a_extremos[extremo1-1])
 		if len(candidatos_a_extremos[extremo1-1])==1: extremos.remove(extremo1)
