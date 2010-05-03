f68bfa3dd42c4a001256eb2df24f07b60f672476
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-05-03 14:12:59 +0000

    aja aja thats the way aja aja i like it

--- algo3/tp2/Ej2/ciudad.h
+++ algo3/tp2/Ej2/ciudad.h
@@ -3,7 +3,6 @@
 #include<stack>
 #include<queue>
 #include<list>
-
 #define pila stack<uint>
 #define cola queue<uint>
 #define lista list<uint>
@@ -15,17 +14,15 @@ typedef unsigned long long int ullint;
 
 using namespace std;
 
+bool ciudad(uint** conexiones, uint n);
+bool dfs_busco_primer_ciclo(uint** conexiones, bool* nodos_ciclo, bool* nodos_ciclo_posible, uint n);
+bool dame_adyacentes_a(uint nodo_actual,uint** conexiones, lista &l_ady, uint n);
+bool dame_arista_libre_ciclo(uint &nodo_salida, uint** conexiones, bool* nodos_ciclo, uint n);
+bool formar_ciclo_desde(uint& nodo_salida, uint** conexiones, bool* nodos_ciclos, uint& cant_nodos_ciclo, uint n);
+uint nodos_en_ciclo(bool* nodos_ciclos, uint n);
+void reseteo_matriz_salvo_ciclo(uint** conexiones, bool* nodos_ciclos, uint n);
+void buscar_y_marcar_ciclo(uint** conexiones, bool* nodos_ciclos, lista &l, uint n);
+void limpiar_array(bool* array, uint n);
 void print_vector(uint* V, uint n);
 void print_matriz(uint** M, uint m, uint n);
 
-bool ciudad(uint** conexiones, uint n);
-bool dfs_busco_primer_ciclo(uint** conexiones, bool* nodos_ciclo, bool* nodos_ciclo_posible, uint n);
-void buscar_y_marcar_ciclo(uint** conexiones, bool* nodos_ciclos, lista &l, uint n);
-bool dame_adyacentes_a(uint nodo_actual,uint** conexiones, lista &l_ady, uint n);
-void reseteo_matriz_salvo_ciclo(uint** conexiones, bool* nodos_ciclos, uint n);
-void limpiar_array(bool* array, uint n);
-
-bool dame_arista_libre_ciclo(uint &nodo_salida, uint** conexiones, bool* nodos_ciclo, uint n);
-bool formar_ciclo_desde(uint& nodo_salida, uint** conexiones, bool* nodos_ciclos, uint& cant_nodos_ciclo, uint n);
-
-uint nodos_en_ciclo(bool* nodos_ciclos, uint n);
