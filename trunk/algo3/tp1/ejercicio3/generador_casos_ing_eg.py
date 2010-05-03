1e91f5fd867dd6e4ea233d32e4de61fe6bb4630b
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-05-03 12:00:44 +0000

    ..

--- trunk/algo3/tp1/ejercicio3/generador_casos_ing_eg.py
+++ trunk/algo3/tp1/ejercicio3/generador_casos_ing_eg.py
@@ -2,44 +2,40 @@
 
 from random import randint, seed
 
-def main():
-    programadores = 1000
+programadores = 1000
 
-    seed()
+seed()
 
-    fIn = open('test_ing_eg.in','w')
+fIn = open('test_ing_eg.in','w')
 
-    ingresos = []
-    egresos = []
-    horas = 0
-    minutos = 0
-    segundos = 0
-    for i in range(programadores):
-        ingresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,i+1)]
+ingresos = []
+egresos = []
+horas = 0
+minutos = 0
+segundos = 0
+for i in range(1,programadores):
+	ingresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,i+1)]
         segundos += 1
         if segundos == 60:
-            segundos = 0
-            minutos += 1
-            if minutos == 60:
-                minutos == 0
-                horas += 1
-    	egresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,i+1)]
+        	segundos = 0
+        	minutos += 1
+        	if minutos == 60:
+        	        minutos == 0
+        	        horas += 1
+    	egresos += ["%.2d:%.2d:%.2d %s"%(horas,minutos,segundos,i)]
         segundos += 1
         if segundos==60:
-            segundos = 0
-            minutos +=1
-            if minutos == 60:
-                minutos =0
-                horas += 1
-
-
-
-
+        	segundos = 0
+        	minutos +=1
+        	if minutos == 60:
+        	        minutos =0
+        	        horas += 1
+	
         fIn.write("%s\n"%(i))
         for p in range(i):
         	fIn.write("%s\n"%(ingresos[p]))
 
        	for p in range(i):
        		fIn.write("%s\n"%(egresos[p]))
-    fIn.write("-1")
+fIn.write("-1")
 
