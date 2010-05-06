54f8108aa00a1dc6157572918ce5b077ea1f5e14
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-05-06 22:06:57 +0000

    VENCIMOS CARAAAJOOOOOOOOOOO

--- algo3/tp2/Ej2/generador_tests.py
+++ algo3/tp2/Ej2/generador_tests.py
@@ -13,8 +13,21 @@ def todos_con_todos(n):
 	return adyacencias
 
 def ciclo_random(n):
-	ciclo = ciclo_minimo(n)
-	ciclo = [ [ x for x in range(1,n+1) if (x in ciclo[i-1]) or (x!=i and random.randint(0,1)==1) ] for i in range(1,n+1) ]
+	ciclo = [ [] for i in range(n) ]
+	for i in range(1,n):
+		ciclo[i-1] += [ k for k in range(i+1,n+1) if k not in ciclo[i-1] and random.randint(1,4)==1 ]
+	for i in range(1,n+1):
+		ciclo[i-1] = [ k for k in range(1,n+1) if k in ciclo[i-1] or i in ciclo[k-1] ]
+	for i in range(1,n+1):
+		if i-1 not in ciclo[i-1] and i!=1:
+			ciclo[i-1].append(i-1)
+		if i+1 not in ciclo[i-1] and i!=n:
+			ciclo[i-1].append(i+1)
+		ciclo[i-1].sort()
+	if n not in ciclo[0]:
+		ciclo[0].append(n)
+	if 1 not in ciclo[n-1]:
+		ciclo[n-1].append(1)
 	return ciclo
 
 def unir_ciclos(ciclo, ciclo2):
@@ -28,18 +41,46 @@ def unir_ciclos(ciclo, ciclo2):
 	ciclo[max+len(ciclo2)-1] += [hasta+1]
 	return ciclo
 
-def generar_input(ciclo):
+def generar_input(ciclos):
 	f = open('test_fc.in','w')
-	f.write( '%d\n'%( len(ciclo) ))
-	for i in range(len(ciclo)):
-		s = reduce( lambda x,y: "%s %s"%(x,y), ciclo[i] )
-		f.write('%d %s\n'%( len(ciclo[i]), s) )
+	for ciclo in ciclos:
+		f.write( '%d\n'%( len(ciclo) ))
+		for i in range(len(ciclo)):
+			s = reduce( lambda x,y: "%s %s"%(x,y), ciclo[i] )
+			f.write('%d %s\n'%( len(ciclo[i]), s) )
 	f.write("-1")
 	f.close()
+
+def generar_instancias( instancias ):
+	generadores = [ todos_con_todos ]
+	ciclos = []
+	for i in instancias:
+		instancia = []
+		for c in i:
+			if type(c)==type(tuple()):
+				n,gen = c
+			else:
+				n = c
+				gen = random.choice(generadores)
+			ciclo = gen(n)
+			if len(instancia)>0:
+				unir_ciclos(instancia,ciclo)
+			else:
+				instancia=ciclo
+		ciclos+=[instancia]
+	return ciclos
+
 
 if __name__=="__main__":
 	#ciclo = unir_ciclos(ciclo_minimo(3),ciclo_minimo(4))
 	#print unir_ciclos(ciclo,todos_con_todos(5))
-	ciclo= ciclo_random(18)
-	print ciclo
-	generar_input(ciclo)
+	n = 15
+	instancias = []
+	for i in range(n):
+		a = random.randint(3,n-5)
+	 	b = random.randint(3,n-3-a)
+		c = n-a-b
+		instancias += [[a,b,c]]
+	ciclos=generar_instancias( instancias  )
+	generar_input( ciclos )
+	
