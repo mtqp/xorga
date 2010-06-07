890a1106e743bc62d2bf83d8d9c3ddb848f2756e
dgrosso <dgrosso@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-06-07 18:46:47 +0000

    Script para ver diferencias entre los métodos

---
+++ algo3/tp3/diferencias.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+import os.path
+from tp3 import cargarInput, cargarOutput, lineaInstancia
+
+if __name__=="__main__":
+	if len(sys.argv) == 4:
+		entrada = sys.argv[1]
+		metodo1 = sys.argv[2]
+		metodo2 = sys.argv[3]
+	else:
+		print "Uso: ",sys.argv[0]," {archivo_entrada} {metodo1} {metodo2}"
+		exit(1)
+	instancias  = cargarInput( entrada )
+	soluciones1 = cargarOutput( "%s/%s.out"%(metodo1,os.path.basename(entrada)[0:-3]) )
+	soluciones2 = cargarOutput( "%s/%s.out"%(metodo2,os.path.basename(entrada)[0:-3]) )
+	for i in range(len(soluciones1)):
+		if len(soluciones1[i]) != len(soluciones2[i]) :
+			print "La %da instancia difiere - tamanyo1: %d  tamanyo2: %d"%(i+1,len(soluciones1[i]),len(soluciones2[i]))
+			print "\t Solucion1: %s"%(str(soluciones1[i]))
+			print "\t Solucion2: %s"%(str(soluciones2[i]))
+			print "\t Linea: %d"%(lineaInstancia(instancias,i))
+			print "-------------------------------------------------------------"
