532e1cdef4f15041c1fb0f7ca5b6389a05d1859d
livornocarla <livornocarla@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-06-26 23:45:05 +0000

    que quede este por dios!!!!

--- trunk/algo3/tp3/tabu_search/tabu_search.cpp
+++ trunk/algo3/tp3/tabu_search/tabu_search.cpp
@@ -52,13 +52,23 @@ int comparar( const void* _a, const void* _b ){
 
 
 void formar_completo(bool* pertenece, int** adyacencia, const int nodo, int& tamanyo, const int n){
+	/*Verifica si la variable nodo es adyacente a todos los vértices de la solución pertenece,
+	*es decir, intenta agrandar la clique con 'nodo'	
+	*/
 	bool forma_completo = true;
-	for( int j = 0 ; j < n ; j++ )
-		if(pertenece[j] && j!= nodo) forma_completo &= adyacencia[nodo][j];
-		if(forma_completo){
-			pertenece[nodo] = true;
-			tamanyo++;
+	for( int j = 0 ; j < n ; j++ ){
+		if(pertenece[j] && j!= nodo) {
+			forma_completo &= adyacencia[nodo][j];
+			O(4);
 		}
+		O(6);
+	}
+	if(forma_completo){
+		pertenece[nodo] = true;
+		tamanyo++;
+		O(3);
+	}
+	O(3);
 }
 
 void rotar(list& l, int c){
@@ -71,130 +81,140 @@ void rotar(list& l, int c){
 
 void formar_completo_lista(bool* pertenece,list& l,pair* d,int* tabu,int** adyacencia,int& tamanyo,const int n){
 	list tmp;
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n;i++){				//intento agregar de mayor a menor grado si todavía no pertenece a la clique y no esta tabú
 		int nodo=d[i].first;
 		if(!pertenece[nodo] && tabu[nodo]==0){
 			bool forma_completo = true;
+			/* Veo que el nodo que pretendo agregar sea adyacente a todos los que están en la clique
+			** No uso la lista para hacer esto porque deberia ir agregando a la lista lo que pasa a pertenecer a la clique y eso no me permite ordenarlos de
+			** menor a mayor entre ellos (los que logro agregar) y a la vez que esten al principio de la clique anterior */
 			for(int j=0;j<n;j++)
 				if(pertenece[j] && j!=nodo) forma_completo &= adyacencia[nodo][j];
 			if(forma_completo){
-				tmp.push_back(nodo);	//tmp de mayor a menor grado
+				tmp.push_back(nodo);	//tmp contiene los nodos que logre agregar a la clique de mayor a menor grado
 				pertenece[nodo]=true;
 				tamanyo++;
 			}
 		}
 	}
-
+	//concateno las listas
 	list::iterator it;
 	it=l.begin();
 	l.splice(it,tmp);
 }
 
-void agrandar_clique(bool* pertenece,list& t, list& l,int** adyacencia,int& tamanyo,const int n){
-	list::iterator it_tabu;
-	it_tabu=t.begin();
-	while(it_tabu!=t.end() && !t.empty()){
-		int nodo=*it_tabu;
-		//cout << "evaluo par: " << par << endl;
-		bool forma_completo = true;
-		for(int j=0;j<n;j++) if(pertenece[j] && j!=nodo) forma_completo &= adyacencia[nodo][j];
-		if(!forma_completo){
-			t.remove(nodo);
-			it_tabu=t.begin();
-			//cout << "no formo completo";
-			//print_lista(t);
-		}
-		else{
-			pertenece[nodo]=true;
-			tamanyo++;
-			it_tabu++;
-			//cout << "formo completo" << endl;
-		}
-	}
-	//concatenar(pertenece,l,t,tamanyo);
-	list::iterator it;
-	it=l.begin();
-	l.splice(it,t);
-}
-
 int constructivo(bool* pertenece, const pair* d, int** adyacencia, const int n){
+	/*construye una clique a partir del de mayor grado*/
 	int tamanyo = 1;
 	int actual = d[0].first;
 	pertenece[actual] = true;
+	O(7);
 	for(int i=0;i<n;i++){
 		int nodo = d[i].first;
 		if(!pertenece[nodo]) formar_completo(pertenece,adyacencia,nodo,tamanyo,n);
+		O(8);
 	}
-	return tamanyo;;
+	O(1);
+	return tamanyo;
 }
 
 int busqueda_local(bool* pertenece, pair* d, int** adyacencia, int n){
 	//Solución inicial
 	int tamanyo = constructivo(pertenece,d,adyacencia,n);
 	if(tamanyo==n || tamanyo==1) return tamanyo;
-	int tam_actual = tamanyo;
+	O(4);
+	int tam_actual 	       = tamanyo;
 	int tam_mejor_vecindad = tam_actual;
 	bool actual[n];
 	bool mejor_vecindad[n];
+	O(5);
 	for(int i=0;i<n;i++){
-		actual[i]=pertenece[i];
+		actual[i] = pertenece[i];
 		mejor_vecindad[i] = pertenece[i];
+		O(8);
 	}
 	
 	bool mejore=true;
+	O(1);
 	while(mejore){
 		mejore=false;
-		for(int i=n-1;i>=0;i--){
+		O(4);
+		for(int i=n-1;i>=0;i--){					//para cada nodo de menor a mayor grado pruebo sacarlo
 			int v1=d[i].first;
-			if(actual[v1]){		//saco un nodo perteneciente a la solución actual
-				actual[v1]=false;
+			O(5);
+			if(actual[v1]){						//si esta en la clique
+				actual[v1]=false;				//lo saco
 				tam_actual--;
-				int nodo=-1;
-				for(int j=0;j<n;j++){
+				int nodo=-1;					//variable para reconstruir la solucion si no es posible mejorarla
+				O(5);
+				for(int j=0;j<n;j++){				//para cada nodo de mayor a menor grado pruebo agregarlos
 					int v2=d[j].first;
-					if(!actual[v2] && v2!=v1){
+					O(5);
+					if(!actual[v2] && v2!=v1){		//si no pertenece a la clique y no es el que saque
 						formar_completo(actual,adyacencia,v2,tam_actual,n);
-						if(actual[v2]) nodo=v2;
+						if(actual[v2]) {
+							nodo=v2;		//me quedo en nodo el ultimo que agrego
+							O(1);
+						}
+						O(2);
 					}
+					O(5);
 				}
-				//cout << "Actual: " << endl;
-				//print_vector(actual,n);
-				if(tam_actual>tam_mejor_vecindad){		//si mejore, actualizo
+				if(tam_actual>tam_mejor_vecindad){		//si mejore con respecto al mejor de la vecindad, actualizo
 					tam_mejor_vecindad=tam_actual;
 					for(int j=0;j<n;j++) mejor_vecindad[j]=actual[j];
-				} else{
-					if(nodo!=-1) actual[nodo]=false;
+					O(5*n+2);
+				} else{						//restauro la solucion anterior
+					if(nodo!=-1){
+						actual[nodo]=false;
+						O(2);
+					}
 					actual[v1]=true;
+					O(3);
 				}
+				O(2);
 			}
+			O(2);
 		}
-		if(tam_mejor_vecindad>tamanyo){		//si mejore, actualizo
+		if(tam_mejor_vecindad>tamanyo){					//si mejore con respecto al optimo actual, actualizo
 			mejore=true;
 			tamanyo=tam_mejor_vecindad;
 			for(int j=0;j<n;j++) pertenece[j]=mejor_vecindad[j];
+			O(5*n+3);
 		}
+		O(2);
 	}
+	O(1);
 	return tamanyo;
 }
-/*
-**CANTIDAD_ITERACIONES es la cantidad maxima de veces que dejo que mejore
-**ITER_TABU es la cantidad de iteraciones que prohibo el atributo
-**CANT_ITER es la cantidad maxima de iteraciones sin mejorar*/
+
+/* CANTIDAD_ITERACIONES es la cantidad maxima de veces que dejo que mejore
+** ITER_TABU es la cantidad de iteraciones que prohibo el atributo
+** CANT_ITER es la cantidad maxima de iteraciones sin mejorar */
 
 int max_clique_actual(bool* pertenece, int** adyacencia, int n){
-	// tupla (nodo, grado)
+	// tupla (nodo, grado)	
 	pair d[n];
-
-	for(int i=0;i<n;i++){
-		pertenece[i]=false;
+	O(1);
+	//inicializo el arreglo de grados
+	O(1);
+	for( int i = 0 ; i < n ; i++ )
+	{
 		d[i].first  = i;
 		d[i].second = 0;
-		for( int j = 0 ; j < n ; j++ )
-			if( adyacencia[i][j] )
+		O(9);
+		for( int j = 0 ; j < n ; j++ ){
+			if( adyacencia[i][j] ){
 				d[i].second++;
+				O(2);
+			}
+			O(4);
+		}
 	}
 
+	//ordeno el arreglo de grados segun los mismo de mayor a menor
 	qsort( d, n, sizeof(pair), &comparar );
+	O(n*n);
 
 	//Solución inicial
 	int tamanyo = busqueda_local(pertenece,d,adyacencia,n);
@@ -202,83 +222,84 @@ int max_clique_actual(bool* pertenece, int** adyacencia, int n){
 	int tam_actual;
 	bool actual[n];
 	int tabu[n];
-	list elem_tabu;
-	list::iterator it_tabu;
 	list clique_actual;
 	
 	bool mejore=true;
 	while(mejore){
 		mejore=false;
 		int cant_iter=max(tamanyo,3);
-			for(int c=0;c<tamanyo;c++){
-				int iteracion=0;
-				tam_actual=tamanyo;
-				for(int i=0;i<n;i++){
-					actual[i]=false;
-					tabu[i]=0;
-				}
-				clique_actual.clear();
-				elem_tabu.clear();
-				for(int i=0;i<n;i++){
-					int vertice=d[i].first;
-					if(pertenece[vertice]){
-						clique_actual.push_back(vertice); //quedan de mayor a menor grado
-						actual[vertice]=true;
-					}
-				}
-				rotar(clique_actual,c);
-				
-				//cout << "Lista actual: ";
-				//print_lista(clique_actual);
-				//cout << "tam_actual: " << tam_actual << endl;
-
-				while(tam_actual!=1 && !mejore && iteracion<n){
-					bool igual_inicial=true;
-					for(int i=0;i<n;i++) igual_inicial&=(pertenece[i]==actual[i]);
-					if(igual_inicial && iteracion!=0) break;
-
-					int nodo=clique_actual.back();
-					//cout << "saco nodo: " << nodo+1 << endl;
-					
-					clique_actual.pop_back();	//saco el de menor grado
-					actual[nodo]=false;
-					tam_actual--;
-					tabu[nodo]=cant_iter;
-					elem_tabu.push_back(nodo);
-
-					//cout << "tam_actual: " << tam_actual << endl;
-					//cout << "Actual";
-					//print_lista(clique_actual);  
-
-					formar_completo_lista(actual,clique_actual,d,tabu,adyacencia,tam_actual,n);
-
-					//cout << "despues de agregar";
-					//print_lista(clique_actual);  
-					//cout << "Tam_actual: " << tam_actual << endl;
-					
-					//cout<< "Tabu: ";
-					//print_lista(elem_tabu);
-					//print_vector(tabu,n);
-					
-					if(tam_actual>tamanyo){		//si mejore, actualizo
-						agrandar_clique(actual,elem_tabu,clique_actual,adyacencia,tam_actual,n);
-						mejore=true;
-						tamanyo=tam_actual;
-						c=tamanyo;
-						for(int k=0;k<n;k++) pertenece[k]=actual[k];
-					}
-					else{
-						for(int j=0;j<n;j++){
-							if(tabu[j]){
-								tabu[j]--;
-								if(tabu[j]==0) elem_tabu.remove(j);
-							}
-						}
-					}
-					iteracion++;
+		/* Empiezo tamanyo veces desde la clique original a no ser que en alguna de las iteraciones logre mejorarla
+		** Cada una de estas veces roto la lista para sacar los nodos en otro orden y asi explorar otros vecinos*/
+		for(int c=0;c<tamanyo;c++){		
+			int iteracion=0;					//iteración controla la máxima cantidad de veces que va a iterar el 'while' sin lograr mejorar
+			//Inicializo todo
+			tam_actual=tamanyo;
+			for(int i=0;i<n;i++) actual[i]=false;
+			clique_actual.clear();
+			for(int i=0;i<n;i++){
+				int vertice=d[i].first;
+				if(pertenece[vertice]){
+					clique_actual.push_back(vertice);	//quedan ordenados de mayor a menor grado
+					actual[vertice]=true;
 				}
 			}
+			rotar(clique_actual,c);
+			
+			//cout << "Lista actual: ";
+			//print_lista(clique_actual);
+			//cout << "tam_actual: " << tam_actual << endl;
+
+			while(tam_actual!=1 && !mejore && iteracion<n){
+			
+				/* Veifico si moviendome por la vecindad vuelvo a la clique inicial, si pasa eso salgo del 'while' para hacer la rotación y 
+				   explorar soluciones distintas */
+				
+				bool igual_inicial=true;
+				for(int i=0;i<n;i++) igual_inicial&=(pertenece[i]==actual[i]);
+				if(igual_inicial && iteracion!=0) break;
+
+				/* Saco el nodo de menor grado (no en todas las iteraciones es el de menor grado sino que es el de menor grado entre los de la misma
+				   antiguedad en la clique) de la clique actual */
+				
+				int nodo=clique_actual.back();					
+				clique_actual.pop_back();			//saco el de menor grado
+				actual[nodo]=false;
+				tam_actual--;
+				tabu[nodo]=cant_iter;
+				
+				//cout << "saco nodo: " << nodo+1 << endl;
+				//cout << "tam_actual: " << tam_actual << endl;
+				//cout << "Actual";
+				//print_lista(clique_actual);  
+				
+				formar_completo_lista(actual,clique_actual,d,tabu,adyacencia,tam_actual,n);
+
+				//cout << "despues de agregar";
+				//print_lista(clique_actual);  
+				//cout << "Tam_actual: " << tam_actual << endl;
+				
+				//cout<< "Tabu: ";
+				//print_vector(tabu,n);
+				
+				if(tam_actual>tamanyo){				//si mejore, actualizo
+					mejore=true;
+					/* Si mejore, reseteo el arreglo 'tabu' e intento agrandar la clique (le doy la oportunidad de estar en la clique a los nodos
+					   que estaban tabú) ya que poder agregar algo sirve porque consigo una clique todavía mayor */
+					for(int i=0;i<n;i++) tabu[i]=0;
+					formar_completo_lista(actual,clique_actual,d,tabu,adyacencia,tam_actual,n);
+					tamanyo=tam_actual;
+					c=tamanyo;				//fuerzo salir del 'for' para empezar por la primero rotación
+					for(int k=0;k<n;k++) pertenece[k]=actual[k];
+				}
+				else{
+					/* Si no pude mejorar, resto la cantidad de iteraciones que le queda a un nodo para dejar de ser tabú */
+					for(int j=0;j<n;j++)
+						if(tabu[j]!=0) tabu[j]--;
+				}
+				iteracion++;
+			}
 		}
+	}
 	return tamanyo;
 }
 
@@ -321,8 +342,8 @@ int main(int argc, char** argv){
 			//for( int z=0; z<n; z++ )
 				//cout << z << "\t" << max_clique_actual(pertenece,adyacencia,n,z,n,n) << endl;
 			cout << max_clique_actual(pertenece,adyacencia,n) << endl;
-			cout << "N";
-			print_res(pertenece,n);
+			//cout << "N";
+			//print_res(pertenece,n);
 		}
 		for(int i=0; i<n; i++){	//libero la memoria
 			delete [] adyacencia[i];
