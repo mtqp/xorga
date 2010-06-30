bf96ae67f439dedd8081bedecd712fa2014ba438
marian.sabianAA <marian.sabianAA@b46ebaae-a8a4-11de-b6f4-e1be98fab775>
2010-06-30 04:29:29 +0000

    epa epa

--- trunk/algo3/tp3/max_clique.sh
+++ trunk/algo3/tp3/max_clique.sh
@@ -1,20 +1,37 @@
-mkdir out #if no esta creado crearlo....
+#pequeño script para ejecutar todos los algoritmos!
+
+if [ ! -d out/ ]; then
+	mkdir out #if no esta creado crearlo....
+	echo "Generando carpeta OUT."
+fi
+
 cd exacto
-make		#if no esta makeado
+if [ ! -e "./exacto" ]; then
+	make
+fi
 ./exacto < ../$1 > ../out/a.out
-echo "Exacto procesó entrada"
+echo "Exacto 		procesó entrada."
+
 cd ..
 cd constructivo
-make		#if no esta makeado
+if [ ! -e "./constructivo" ]; then
+	make
+fi
 ./constructivo < ../$1 > ../out/const.out #corregir los nombres q estan como la mona
-echo "Constructivo procesó entrada"
+echo "Constructivo 	procesó entrada."
+
 cd ..
 cd busqueda_local
-make		#if no esta makeado
+if [ ! -e "./busqueda_local" ]; then
+	make
+fi
 ./busqueda_local < ../$1 > ../out/busq_lo.out
-echo "Búsqueda local procesó entrada"
+echo "Búsqueda Local  procesó entrada."
+
 cd ..
 cd tabu_search
-make		#if no esta makeado
+if [ ! -e "./tabu_search" ]; then
+	make
+fi
 ./tabu_search < ../$1 > ../out/tabu.out
-echo "Tabú Search procesó entrada"
+echo "Tabú Search 	procesó entrada."
